1397c8556ae8a626aa2dc165dafda259eece18a9
earthctzn <earthctzn1@gmail.com>
2019-05-24 14:47:01 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+attr_reader :balance, :status, :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    if @status = "open" && @balance > 0
+      valid
+    else
+      invalid
+    end
+  end
+
+  
+
+
 
 end
