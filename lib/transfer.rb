633f0b86fb72a62b16a873b01402467f4bb07b78
earthctzn <earthctzn1@gmail.com>
2019-05-24 16:19:47 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 
   def execute_transaction
     if sender.valid? && receiver.valid? && sender.balance > @amount
-    elsif self.status == "pending"
+    elsif self.status != "complete"
 
       sender.balance -= @amount
       receiver.balance += @amount
