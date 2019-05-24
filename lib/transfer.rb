6d897affb7f1fdb6f0b52298b767c4c37af948d6
earthctzn <earthctzn1@gmail.com>
2019-05-24 16:30:53 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,12 @@ class Transfer
   end
 
   def reverse_transfer
+    if self.status == "complete"
+
+      receiver.balance -= @amount
+      sender.balance += @amount
+      self.status = "complete"
+    end 
 
   end
 
